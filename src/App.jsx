43d7319a649c1ae09c1b46d454da2142c9f94150
pejhanbio-cmd <pejhan79s@gmail.com>
2026-09-17 16:28:35 +0330

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+     return (
+       <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Vazirmatn' }}>
+         <h1 style={{ color: '#2D5016' }}>🌿 به biology-academy خوش آمدید 🌿</h1>
+         <p>سایت با موفقیت متصل شد!</p>
+       </div>
+     )
+   }
+
+   export default App
